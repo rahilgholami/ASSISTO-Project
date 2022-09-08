e141654e8b503c3d47ae7c90c115955ef795228d
rahilgholami <35683197+rahilgholami@users.noreply.github.com>
2022-09-08 13:13:15 +0200

    Update utils_d.py

--- src/utils_d.py
+++ src/utils_d.py
@@ -1,6 +1,18 @@
-import torch
-from torch.utils import data
-import torch.nn.functional as F
+# Copyright (c) Rahil Gholamipoor
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+## Libraries
 import math
 import pickle
 import numpy as np
@@ -13,9 +25,12 @@ from sklearn.utils import shuffle
 from datetime import datetime, timedelta
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve, auc
-
-
 from openpyxl.utils import get_column_letter, column_index_from_string
+
+## Torch
+import torch
+from torch.utils import data
+import torch.nn.functional as F
 
 def get_data(x):
     bs, _, l = x.size()
